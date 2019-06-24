920273e1afebc488c05bda82be3b4b4c78ccf5c7
Masaki Muranaka <monaka@monami-ya.com>
2019-06-24 10:57:37 +0900

    Just upgrading Yarn

--- dockerfiles/theia-dev/src/entrypoint.sh
+++ dockerfiles/theia-dev/src/entrypoint.sh
@@ -26,7 +26,4 @@ if ! grep -Fq "${USER_ID}" /etc/passwd; then
     sed "s/\${GROUP_ID}/${GROUP_ID}/g" > /etc/group
 fi
 
-# Avlid build errors caused by Yarn's parallel build
-node-gyp install
-
 exec "$@"
